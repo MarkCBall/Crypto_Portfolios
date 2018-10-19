a7eb9566a887e1727c69bd46fe7babea76201fe4
MarkCBall <moball@hotmail.com>
2018-10-19 09:40:36 -0400

    start to fix up blog/:slug

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -6,17 +6,8 @@ var posts = [
     tags: ["blockchain", "web development"],
     created_at: "2018-10-16T23:45:24.565Z",
     slug: "my-first-blog-post",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-               tempor incididunt ut labore et dolore magna aliqua. Nibh tellus molestie nunc \
-               non blandit. Diam in arcu cursus euismod. Imperdiet nulla malesuada pellentesque \
-               elit eget gravida. Sollicitudin tempor id eu nisl nunc mi ipsum. Tincidunt eget nullam \
-               non nisi est sit amet facilisis. Volutpat est velit egestas dui id ornare arcu odio. \
-               Nulla at volutpat diam ut venenatis tellus in metus. Commodo odio aenean sed adipiscing diam. \
-               Tellus mauris a diam maecenas sed. Nisl rhoncus mattis rhoncus urna neque viverra justo. \
-               Tempor orci dapibus ultrices in iaculis nunc. Sit amet nisl suscipit adipiscing.",
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\
-    	        tempor incididunt ut labore et dolore magna aliqua.Nibh tellus molestie nunc\
-              non blandit.",
+    content: "content my first blog post",
+    summary: "summary my first blog post",
     image_url: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/01/Blockchain-Funds.png"
   },
   {
@@ -24,17 +15,8 @@ var posts = [
   	tags: ["smart contracts"],
   	created_at: "2018-10-16T23:45:24.565Z",
   	slug: "my-second-blog-post",
-  	content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-               tempor incididunt ut labore et dolore magna aliqua. Nibh tellus molestie nunc \
-               non blandit. Diam in arcu cursus euismod. Imperdiet nulla malesuada pellentesque \
-               elit eget gravida. Sollicitudin tempor id eu nisl nunc mi ipsum. Tincidunt eget nullam \
-               non nisi est sit amet facilisis. Volutpat est velit egestas dui id ornare arcu odio. \
-               Nulla at volutpat diam ut venenatis tellus in metus. Commodo odio aenean sed adipiscing diam. \
-               Tellus mauris a diam maecenas sed. Nisl rhoncus mattis rhoncus urna neque viverra justo. \
-               Tempor orci dapibus ultrices in iaculis nunc. Sit amet nisl suscipit adipiscing.",
-  	summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\
-    	        tempor incididunt ut labore et dolore magna aliqua.Nibh tellus molestie nunc\
-              non blandit.",
+  	content: "content my second blog post",
+  	summary: "summary my second blog post",
   	image_url: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/01/Blockchain-Funds.png"
   }]
 
