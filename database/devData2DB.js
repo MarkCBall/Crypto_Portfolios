e6d780d2d7feae7234fb2a083cfe8509e316ab8b
MarkCBall <moball@hotmail.com>
2018-10-20 23:34:02 -0400

    hrefs from database done

--- database/devData2DB.js
+++ database/devData2DB.js
@@ -1,163 +1,72 @@
-// // let mongoose = require('mongoose');
 
 
-// const database = () => {
-// 	mongoose.connect('mongodb://moball:123456a@ds047335.mlab.com:47335/gbc-blogapp-db')
-// 		.then(() => { console.log(`Connected to mongodb`)})
-// 		.catch(err => { console.log(err)});
-// };//end database stuff
-
-// var userSchema = mongoose.Schema(
-//     {
-//         userName: String,
-//     });
-
-// var Users = mongoose.model('Users', userSchema);
-
-// var user1 = new Users({userName:"Mark"});
-// var user2 = new Users({userName:"Dylan"});
-
-// user1.save(function (err) {
-//     if (err) return handleError(err);
-//     // saved!
-// });
-
-// user2.save(function (err) {
-//     if (err) return handleError(err);
-//     // saved!
-// });  
-
-/// ahhh, no clue how to do this...
-
-///////////////////////////////////////////
 const mongoose = require('mongoose');
-//const Post = require('../models/post');
 const dbConnect = require('./database');
 
-
-
-
-
-
-//let mongoose = require('mongoose');
-
-let postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    tags: Array,
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    slug: String,
-    content: {
-        type: String,
-        required: true
-    },
-    summary: String,
-    image_url: String
+//put into separate file
+let testSchema = new mongoose.Schema({
+    userName: { type: String, required: true },
+    portfolioName: { type: String, required: true },
+    tokenTicker: { type: String, required: true },
+    tokenName: { type: String, required: true },
+    tokenAmount: { type: Number, required: true }
 });
 
-let Post = mongoose.model('Post', postSchema);
-
-
-
-
-
-
+let Post = mongoose.model('Post', testSchema);
 
 dbConnect();
 
 var posts = [
     new Post({
-        title: "My First Blog Post",
-        tags: ["blockchain", "web development"],
-        created_at: "2018-10-16T23:45:24.565Z",
-        slug: "my-first-blog-post",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-               tempor incididunt ut labore et dolore magna aliqua. Nibh tellus molestie nunc \
-               non blandit. Diam in arcu cursus euismod. Imperdiet nulla malesuada pellentesque \
-               elit eget gravida. Sollicitudin tempor id eu nisl nunc mi ipsum. Tincidunt eget nullam \
-               non nisi est sit amet facilisis. Volutpat est velit egestas dui id ornare arcu odio. \
-               Nulla at volutpat diam ut venenatis tellus in metus. Commodo odio aenean sed adipiscing diam. \
-               Tellus mauris a diam maecenas sed. Nisl rhoncus mattis rhoncus urna neque viverra justo. \
-               Tempor orci dapibus ultrices in iaculis nunc. Sit amet nisl suscipit adipiscing.",
-        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\
-    	        tempor incididunt ut labore et dolore magna aliqua.Nibh tellus molestie nunc\
-              non blandit.",
-        image_url: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/01/Blockchain-Funds.png"
+        userName: "Mark",
+        portfolioName: "P1",
+        tokenTicker: "btc",
+        tokenName: "bitcoin",
+        tokenAmount: 100
     }),
-
-    new Post ({
-        title: "My Second Blog Post",
-        tags: ["smart contracts"],
-        created_at: "2018-10-16T23:45:24.565Z",
-        slug: "my-second-blog-post",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-               tempor incididunt ut labore et dolore magna aliqua. Nibh tellus molestie nunc \
-               non blandit. Diam in arcu cursus euismod. Imperdiet nulla malesuada pellentesque \
-               elit eget gravida. Sollicitudin tempor id eu nisl nunc mi ipsum. Tincidunt eget nullam \
-               non nisi est sit amet facilisis. Volutpat est velit egestas dui id ornare arcu odio. \
-               Nulla at volutpat diam ut venenatis tellus in metus. Commodo odio aenean sed adipiscing diam. \
-               Tellus mauris a diam maecenas sed. Nisl rhoncus mattis rhoncus urna neque viverra justo. \
-               Tempor orci dapibus ultrices in iaculis nunc. Sit amet nisl suscipit adipiscing.",
-        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\
-    	        tempor incididunt ut labore et dolore magna aliqua.Nibh tellus molestie nunc\
-              non blandit.",
-        image_url: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/01/Blockchain-Funds.png"
-    }),
-
-    new Post ({
-        title: "My Third Blog Post",
-        tags: ["dapps"],
-        created_at: "2018-09-16T23:45:24.565Z",
-        slug: "my-third-blog-post",
-        content: "Asdf ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-               tempor incididunt ut labore et dolore magna aliqua. Nibh tellus molestie nunc \
-               non blandit. Diam in arcu cursus euismod. Imperdiet nulla malesuada pellentesque \
-               elit eget gravida. Sollicitudin tempor id eu nisl nunc mi ipsum. Tincidunt eget nullam \
-               non nisi est sit amet facilisis. Volutpat est velit egestas dui id ornare arcu odio. \
-               Nulla at volutpat diam ut venenatis tellus in metus. Commodo odio aenean sed adipiscing diam. \
-               Tellus mauris a diam maecenas sed. Nisl rhoncus mattis rhoncus urna neque viverra justo. \
-               Tempor orci dapibus ultrices in iaculis nunc. Sit amet nisl suscipit adipiscing.",
-        summary: "Asdf ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\
-    	        tempor incididunt ut labore et dolore magna aliqua.Nibh tellus molestie nunc\
-              non blandit.",
-        image_url: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/01/Blockchain-Funds.png"
-    }),
-
     new Post({
-        title: "My Fourth Blog Post",
-        tags: ["javascript"],
-        created_at: Date.now(),
-        slug: "my-fourth-blog-post",
-        content: "Asdfsdfdsfdf ipsdfdssdfum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-               tempor incididunt ut labore et dolore magna aliqua. Nibh tellus molestie nunc \
-               non blandit. Diam in arcu cursus euismod. Imperdiet nulla malesuada pellentesque \
-               elit eget gravida. Sollicitudin tempor id eu nisl nunc mi ipsum. Tincidunt eget nullam \
-               non nisi est sit amet facilisis. Volutpat est velit egestas dui id ornare arcu odio. \
-               Nulla at volutpat diam ut venenatis tellus in metus. Commodo odio aenean sed adipiscing diam. \
-               Tellus mauris a diam maecenas sed. Nisl rhoncus mattis rhoncus urna neque viverra justo. \
-               Tempor orci dapibus ultrices in iaculis nunc. Sit amet nisl suscipit adipiscing.",
-        summary: "Asfsdfdsfsddf isdfsdfsdfsdpsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\
-    	        tempor incididunt ut labore et dolore magna aliqua.Nibh tellus molestie nunc\
-              non blandit.",
-        image_url: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/01/Blockchain-Funds.png"
+        userName: "Mark",
+        portfolioName: "P1",
+        tokenTicker: "eth",
+        tokenName: "etherum",
+        tokenAmount: 1300
+    }),
+    new Post({
+        userName: "Mark",
+        portfolioName: "P2",
+        tokenTicker: "xrp",
+        tokenName: "ripple",
+        tokenAmount: 3300
+    }),
+    new Post({
+        userName: "Dyla",
+        portfolioName: "MyP",
+        tokenTicker: "xrp",
+        tokenName: "ripple",
+        tokenAmount: 3300
+    }),
+    new Post({
+        userName: "Dylan",
+        portfolioName: "MyP",
+        tokenTicker: "bcc",
+        tokenName: "Bitcoin Cash",
+        tokenAmount: 39300
     })
 ]
 
 // seeding to mongodb
 var done = 0;
 
-for (var i = 0; i < posts.length; ++i) {
-    posts[i].save((err, result) => {
-        done++;
-        if (done === posts.length) {
-            mongoose.disconnect();
-        }
-    });
-}
+// for (var i = 0; i < posts.length; ++i) {
+//     posts[i].save((err, result) => {
+//         done++;
+//         if (done === posts.length) {
+//             mongoose.disconnect();
+//         }
+//     });
+// }
 
-
+Post.distinct('userName', function(error, users) {
+    for (var i=0;i<users.length;i++)
+        console.log(users[i])
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+const dbConnect = require('./../database/database');
+dbConnect();
+
+///////////
+
+//put into separate file
+let testSchema = new mongoose.Schema({
+    userName: { type: String, required: true },
+    portfolioName: { type: String, required: true },
+    tokenTicker: { type: String, required: true },
+    tokenName: { type: String, required: true },
+    tokenAmount: { type: Number, required: true }
+});
+let Post = mongoose.model('Post', testSchema);
+dbConnect();
+//////////
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+ 
+    Post.distinct('userName', function(error, users) {
+        res.render('index', {usersArray:users});
+    });
+
+  
 });
 
 router.get('/:slug',function(req, res, next) {
