246fbdbcba2366156ae4007e944295b02d6aa90c
MarkCBall <moball@hotmail.com>
2018-11-16 11:21:36 -0500

    fix update and reorganize

--- routes/index.js
+++ routes/index.js
@@ -10,84 +10,42 @@ const Post = require("./../models/coins");
 
 
 
-////////////ADD TO DATABASE//////////////////////////////////
-/////////////////////////////////////////////////////////////
+//////User Screen Routers
+router.get('/', function(req, res, next) {
+    Post.distinct('userName', function(error, users) {
+        res.render('index', {usersArray:users});
+    });
+});
+
 router.post('/addUser', function(req, res) {
     new Post({userName: req.body.userToAdd,portfolioName: "Default",tokenTicker: "btc",tokenName: "Bitcoin",tokenAmount: 0}).save();
     //res.send(200);//tells the browser that the data was send successfully
     res.redirect("/"+req.body.userToAdd)
-  });
+});
 
-/////////////////////////////////////////////////////////////
-  router.post('/addPortfolio', function(req, res) {
-    new Post({userName: req.body.userName,portfolioName: req.body.portfolioToAdd,tokenTicker: "btc",tokenName: "Bitcoin",tokenAmount: 0}).save();
-    //res.send(200);//tells the browser that the data was send successfully
-    res.redirect("/"+req.body.userName+"/"+req.body.portfolioToAdd)
-  });
-/////////////////////////////////////////////////////////////
-  router.post('/addCoin', function(req, res) {
-    new Post({
-        userName: req.body.userName,
-        portfolioName: req.body.portfolioName,
-        tokenTicker: req.body.tokenTickerToAdd,
-        tokenName: req.body.tokenNameToAdd,
-        tokenAmount: req.body.tokenAmountToAdd
-    }).save();
-
-        res.redirect("/"+req.body.userName+"/"+req.body.portfolioName)
-  });
-
-////////////UPDATE//////////////////////////////
-/////////////////////////////////////////////////////////////
-  router.post('/updateAmount', function(req, res){
-
-    Post.findOneAndUpdate({userName:req.body.userName,portfolioName:req.body.portfolioName,tokenTicker:req.body.tokenTicker},{$set:{tokenAmount:req.body.newAmount}},{new: true}, function(error,doc){
-        if(error){
-            console.log(error)
-        }
-        console.log(doc)
-    });
-    res.redirect("/"+req.body.userName+"/"+req.body.portfolioName)
-  });
-
-  router.put('/:userToDelete/:portToDelete/:coinToDelete', function(req, res, next){
-    Post.findOneAndUpdate({
-        userName:req.params.userToDelete,
-        portfolioName:req.params.portToDelete,
-        tokenTicker:req.params.coinToDelete
-        },
-        {$set:{tokenAmount:req.body.newAmount}},
-        {new: true}, 
-        function(error,doc){}
-    )
-    console.log(req.body.newAmount)
-    //Post.deleteMany({
-        //console.log(req.params.userToDelete)
-        //portfolioName:req.params.portToDelete,
-        //tokenTicker:req.params.coinToDelete
-    // },  function(err){
-    //         if (err) return next(err);
-    //         res.send();
-    });
-
-
-
-
-//DELETE COIN ROUTER  -- error handling to be completed
-//will delete all coins with ticker
-  router.delete('/:userToDelete/:portToDelete/:coinToDelete', function(req, res, next){
+router.delete('/:userToDelete', function(req, res, next){
     Post.deleteMany({
-        userName:req.params.userToDelete,
-        portfolioName:req.params.portToDelete,
-        tokenTicker:req.params.coinToDelete
+        userName:req.params.userToDelete
     },  function(err){
             if (err) return next(err);
             res.send();
     });
+});
+
+
+//////Portfolio Screen Routers
+router.get('/:slug',function(req, res, next) {
+    Post.distinct('portfolioName',{userName:req.params.slug}, function(error, portfolios) {
+    res.render('portDisp', {path1: req.params.slug, portfolios:portfolios} ); 
+    });
+});
+
+router.post('/addPortfolio', function(req, res) {
+    new Post({userName: req.body.userName,portfolioName: req.body.portfolioToAdd,tokenTicker: "btc",tokenName: "Bitcoin",tokenAmount: 0}).save();
+    res.redirect("/"+req.body.userName+"/"+req.body.portfolioToAdd)
   });
 
-//DELETE PORTFOLIO ROUTER  -- error handling to be completed
-  router.delete('/:userToDelete/:portToDelete', function(req, res, next){
+router.delete('/:userToDelete/:portToDelete', function(req, res, next){
     Post.deleteMany({
         userName:req.params.userToDelete,
         portfolioName:req.params.portToDelete
@@ -95,39 +53,51 @@ router.post('/addUser', function(req, res) {
             if (err) return next(err);
             res.send();
     });
-  });
-
-  //DELETE USER ROUTER - error handling to be completed
-  router.delete('/:userToDelete', function(req, res, next){
-    Post.deleteMany({
-        userName:req.params.userToDelete
-    },  function(err){
-            if (err) return next(err);
-            res.send();
-    });
-  });
-
-
-
-////////////DISPLAY FROM DATABASE/////////////////////////////
-/////////////////////////////////////////////////////////////
-router.get('/', function(req, res, next) {
-    Post.distinct('userName', function(error, users) {
-        res.render('index', {usersArray:users});
-    });
 });
 
-router.get('/:slug',function(req, res, next) {
-    Post.distinct('portfolioName',{userName:req.params.slug}, function(error, portfolios) {
-    res.render('portDisp', {path1: req.params.slug, portfolios:portfolios} ); 
-    });
 
-});
-
+///////Coin Screen Routers
 router.get('/:slug/:slug2',function(req, res, next) {
     Post.find({userName:req.params.slug, portfolioName:req.params.slug2}, function(error, coins){
     res.render('coinsDisp', {path1: req.params.slug, path2:req.params.slug2, coins:coins} );
     });
+});
+
+router.post('/addCoin', function(req, res) {
+    new Post({
+        userName: req.body.userName,
+        portfolioName: req.body.portfolioName,
+        tokenTicker: req.body.tokenTickerToAdd,
+        tokenName: req.body.tokenNameToAdd,
+        tokenAmount: req.body.tokenAmountToAdd
+    }).save();
+        res.redirect("/"+req.body.userName+"/"+req.body.portfolioName)
+});
+
+router.delete('/:userToDelete/:portToDelete/:coinToDelete', function(req, res, next){
+    Post.deleteMany({
+        userName:req.params.userToDelete,
+        portfolioName:req.params.portToDelete,
+        tokenTicker:req.params.coinToDelete
+    },  function(err){
+            if (err) return next(err);
+            res.send();
+    });
+});
+
+router.put('/:userToUpdate/:portToUpdate/:coinToUpdate', function(req, res, next){
+    Post.findOneAndUpdate({
+        userName:req.params.userToUpdate,
+        portfolioName:req.params.portToUpdate,
+        tokenTicker:req.params.coinToUpdate
+        },
+        {$set:{tokenAmount:req.body.newAmount}},
+        function(err){
+            if (err) return next(err);
+            res.send();
+        }
+    )
+    //Why does this automatically refresh the page but delete doesn't?
 });
 
 module.exports = router;
