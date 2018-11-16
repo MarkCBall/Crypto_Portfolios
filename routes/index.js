5f9c9d8ad0a166d559cb296f8a9081791d26aaf2
MarkCBall <moball@hotmail.com>
2018-11-16 14:22:19 -0500

    sketchy price API started

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const mongoose = require('mongoose');
-const dbConnect = require('./../database/database');
-//dbConnect();
-
 const Post = require("./../models/coins");
-
-
-
 
 //////User Screen Routers
 router.get('/', function(req, res, next) {
